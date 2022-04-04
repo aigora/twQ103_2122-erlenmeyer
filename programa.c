375fef3b90e581a89a7f54b2b99c6a1d54aecbb1
susanaramos02 <80400988+susanaramos02@users.noreply.github.com>
2022-04-04 10:48:05 +0200

    Update programa.c

--- programa.c
+++ programa.c
@@ -1,3 +1,30 @@
 #include <stdio.h>
 
+int main(){
+	int opcion;
 
+    do
+    {
+		printf("Bienvenido al Erlenmeyer Dorado valiente.\n");
+		printf("1. JUGAR\n");
+		printf("2. RANKING\n");
+		printf("3. SALIR");
+        do
+        {
+            printf("Introduzca opcion:");
+            scanf( "%d", &opcion);
+        } while ( opcion < '1' || opcion > '3' );
+
+        switch ( opcion )
+        {
+            case '1': 
+                      break;
+
+            case '2': 
+                      break;
+        }
+
+    } while ( opcion != '3' );
+
+	return 0;
+}

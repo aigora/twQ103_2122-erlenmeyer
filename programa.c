94985924dfb83a27f77fef0063442a043e4be762
susanaramos02 <80400988+susanaramos02@users.noreply.github.com>
2022-04-08 23:09:20 +0200

    Update programa.c

--- programa.c
+++ programa.c
@@ -6,14 +6,15 @@ int main(){
     do
     {
 		printf("Bienvenido al Erlenmeyer Dorado valiente.\n");
-		printf("1. JUGAR\n");
-		printf("2. RANKING\n");
-		printf("3. SALIR");
+		printf("1. REGLAS\n");
+		printf("2. JUGAR\n");
+		printf("3. RANKING\n");
+		printf("4. SALIR");
         do
         {
             printf("Introduzca opcion:");
             scanf( "%d", &opcion);
-        } while ( opcion < '1' || opcion > '3' );
+        } while ( opcion < '1' || opcion > '4' );
 
         switch ( opcion )
         {
@@ -22,9 +23,11 @@ int main(){
 
             case '2': 
                       break;
+            case '3':
+                      break;            	
         }
 
-    } while ( opcion != '3' );
+    } while ( opcion != '4' );
 
 	return 0;
 }
